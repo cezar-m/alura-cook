618df3629a6b00851fd86af246aa14014fdbb6b3
cezar-m <103004018+cezar-m@users.noreply.github.com>
2022-08-13 21:13:25 -0300

    Projeto Alura-Cook

---
+++ src/routes/index.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import Titulo from '$components/compartilhados/Titulo.svelte';
+	import Categoria from '$components/paginas/index/Categoria.svelte';
+	import Tag from '$components/compartilhados/Tag.svelte';
+	import TagLink from '$components/compartilhados/TagLink.svelte';
+
+	import categorias from '$lib/json/categorias.json';
+		
+	import { minhaLista } from "$lib/stores/minhaLista";
+	import { beforeNavigate } from "$app/navigation";
+	
+	$: listaVazia = $minhaLista.length === 0;
+	
+	beforeNavigate((navigation) => {
+		if(listaVazia && navigation.to?.pathname === '/receitas') {
+			navigation.cancel();
+		}
+	})
+</script>
+
+<svelte:head>
+	<title>Alura Cook</title>
+</svelte:head>
+
+<main>
+	<Titulo tag='h1'>Ingredientes</Titulo>
+	
+	<div class="info">
+		<p>Selecione abaixo os ingredientes que você deseja usar nesta refeição:</p>
+		<p>*Atenção: consideramos que você tenha em casa sal, pimenta e água.</p>
+	</div>
+	
+	<ul class="categorias">
+		{#each categorias as categoria (categoria.nome)}
+			<li>
+				<Categoria 
+					{categoria} 
+				/>
+			</li>
+		{/each}
+	</ul>
+
+	<div class="buscar-receitas">
+		<TagLink href="/receitas" desabilitada={listaVazia}>
+			Buscar receitas!
+		</TagLink>
+	</div>
+</main>
+	
+<style>
+	.info {
+		margin-bottom: 3.375rem;
+	}
+	
+	.info > p {
+		line-height: 2rem;
+	} 
+	
+	.categorias {
+		margin-bottom: 4.6875rem;
+		
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		gap: 1.5rem;
+	}
+	
+	.buscar-receitas {
+		display: flex;
+		justify-content: center;
+	}
+</style>
